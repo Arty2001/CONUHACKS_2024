9bee4bedd2b71c84224eb5c60e0329347185bb3a
Abilash10 <abilashsasi10@gmail.com>
2024-01-20 15:19:39 -0500

    abilash front end

--- frontend/src/components/QuestionPage.tsx
+++ frontend/src/components/QuestionPage.tsx
@@ -6,16 +6,23 @@ interface QuestionPageProps{
 
 function QuestionPage({page}: QuestionPageProps){
     return (
+        <div style={{width:'100%'}}>
+            <h2 style={{ marginLeft: '10px' }}>{page} of 4</h2>
         <div style={{display:'flex' , width:'100%',height: 5, alignSelf: 'flex-start'}}>
-            <div style={{backgroundColor: page > 0 ? "green" : 'grey', flexGrow:1, margin: '0 5px'}}>
-                <h2> Page 1 of 4</h2>
+            <div 
+            style={{backgroundColor: page > 0 ? "green" : 'grey', flexGrow:1, margin: '0 5px', borderRadius: '10px', boxShadow: page === 1 ? '0 0 10px rgba(0, 255, 0, 0.5)' : 'none'}}>
+            {/* <h2> Page 1 of 4</h2> */}
             </div>
-            <div style={{backgroundColor: page > 1 ? "green" : 'grey', flexGrow:1, margin: '0 5px'}}>
+            <div style={{backgroundColor: page > 1 ? "green" : 'grey', flexGrow:1, margin: '0 5px', borderRadius: '10px', boxShadow: page === 2 ? '0 0 10px rgba(0, 255, 0, 0.5)' : 'none'}}>
+            {/* <h2> Page 2 of 4</h2> */}
             </div>
-            <div style={{backgroundColor: page > 2 ? "green" : 'grey', flexGrow:1, margin: '0 5px'}}>
+            <div style={{backgroundColor: page > 2 ? "green" : 'grey', flexGrow:1, margin: '0 5px', borderRadius: '10px', boxShadow: page === 3 ? '0 0 10px rgba(0, 255, 0, 0.5)' : 'none'}}>
+            {/* <h2> Page 3 of 4</h2> */}
             </div>
-            <div style={{backgroundColor: page > 3 ? "green" : 'grey', flexGrow:1, margin: '0 5px'}}>
+            <div style={{backgroundColor: page > 3 ? "green" : 'grey', flexGrow:1, margin: '0 5px', borderRadius: '10px', boxShadow: page === 4 ? '0 0 10px rgba(0, 255, 0, 0.5)' : 'none'}}>
+            {/* <h2> Page 4 of 4</h2> */}
             </div>
+        </div>
         </div>
     )
 }
