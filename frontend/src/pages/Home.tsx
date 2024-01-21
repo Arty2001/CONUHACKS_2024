174bbdbc7769ee667676caac8fc223ec9ddccef7
Abilash10 <abilashsasi10@gmail.com>
2024-01-20 19:37:51 -0500

    abilash

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,10 +10,10 @@ import { Box, styled } from '@mui/system';
 function Home() {
   const [page, setPage] = useState<number>(0);
   const [name, setName] = useState('');
-  const [age, setAge] = useState<any>(18);
+  const [age, setAge] = useState<any>(0);
   const [annualIncome, setAnnualIncome] = useState<any>(0);
   const [houseValue, setHouseValue] = useState<any>(0)
-  const [financialGoal, setfinancialGoal] = useState('Get My Dream House');
+  const [financialGoal, setfinancialGoal] = useState('--Select--');
   const [debts, setDebts] = useState<any>([])
   const [rentBoolean, setRentBoolean]= useState<boolean>(false)
   const [rent, setRent] = useState<any>(0)
@@ -24,28 +24,33 @@ function Home() {
   const [RRSP , setRRSP] = useState<any>(0);
 
   return (<>
-    <div style={{display:'flex', flexDirection:'column', width: '100%', height: '100vh', justifyContent: 'center', alignItems:'center'}}>
+    <div style={{margin: "auto", display:'flex', flexDirection:'column', width: '100%', height: '100vh', justifyContent: 'center', alignItems:'center'}}>
       {page !== 0 && <QuestionPage page={page}/>}
       <AnimatePresence>
-      {page ===0 && <div>
+      {page ===0 && <div style={{color: 'white', backgroundColor: 'black', margin: "auto", display:'flex', flexDirection:'column', width: '100%', height: '100vh', justifyContent: 'center', alignItems:'center'}}>
         <h2>Welcome to your financial journey! </h2>
         <button onClick={()=>{setPage((prev)=>{return prev+1})}}> Starts</button>
       </div> }
       <div style={{width:"100%", height:500}}>
-      {page ===1 && <div>
+      {page ===1 && <div style={{ margin: 'auto', width: '50%', display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '60px', border: '1px solid grey', padding: '10px', borderRadius: '40px' }}>
+        <h2>Personal Information</h2>
         <TextField
           label={"Name"}
           value={name}
           onChange={(event) => setName(event.currentTarget.value)}
+          style={{ margin: '10px', width: '300px' }}
         />
         <TextField
           inputProps={{ type: 'number'}}
+          label="Age"
           placeholder={"Age"}
           defaultValue={age}
           onChange={(event:any) => setAge(event.currentTarget.value)}
+          style={{ margin: '10px', width: '300px' }}
         />
         <TextField 
           inputProps={{ type: 'number'}}
+          label = "Annual Income"
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -55,12 +60,14 @@ function Home() {
           }}
           defaultValue={annualIncome}
           onChange={(event:any) => setAnnualIncome(event.currentTarget.value)}
+          style={{ margin: '10px', width: '300px' }}
          />
         <div style={{display:'flex', width:'100%', justifyContent:'flex-end'}}>
           <Button onClick={()=>{setPage((prev)=>{return prev+1})}}> Next</Button>
         </div>
      </div> }
-      {page ===2 && <div>
+      {page ===2 && <div style={{ margin: 'auto', width: '30%', display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '60px', border: '1px solid grey', padding: '10px', borderRadius: '40px' }}>
+      <h2>Financial Goal</h2>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
@@ -68,6 +75,7 @@ function Home() {
           label="Financial Goal"
           onChange={(event:any) => setfinancialGoal(event.target.value as string)}
         >
+          <MenuItem value={'--Select--'}>--Select--</MenuItem>
           <MenuItem value={'Get My Dream House'}>Get My Dream House</MenuItem>
           <MenuItem value={'PD'}>Pay off Debt</MenuItem>
           <MenuItem value={'RP'}>Retirement plan</MenuItem>
@@ -89,7 +97,7 @@ function Home() {
          />
         }
         <div style={{display:'flex', width:'100%', justifyContent:'space-between'}}>
-          <Button onClick={()=>{setPage((prev)=>{return prev+1})}}> Previous </Button>
+          <Button onClick={()=>{setPage((prev)=>{return prev-1})}}> Previous </Button>
           <Button onClick={()=>{setPage((prev)=>{return prev+1})}}> Next</Button>
         </div></div> }
       {page ===3 && <div>
